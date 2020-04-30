ff0c8529974ae5a76d01fcb15be09c70d737ab45
Artiom <sinigur2006@gmail.com>
2020-04-30 13:09:04 -0400

    Adding heroku build setup

--- server/server.js
+++ server/server.js
@@ -41,4 +41,17 @@ app.use(passport.session())
 app.use(appRouters)
 app.use(authRouters)
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file.
+    app.use(express.static('client/build'))
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 app.listen(PORT, () => console.info('Server is up on port ' + PORT))
